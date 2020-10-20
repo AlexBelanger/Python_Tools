60d16aa99d54feb202fab31c5a04378d33a95806
Alex Belanger <a.belanger89@gmail.com>
2020-10-20 15:32:43 -0400

    Revert "Created separate file and filemanager classes"
    This reverts commit 66207212f3f920c53a73347940c5451ff38662f2.

--- filemanager/filemanager.py
+++ filemanager/filemanager.py
@@ -1,7 +1,96 @@
 import os
 import fnmatch
-import File as File
 
+class File():
+    '''
+    File object
+    '''
+    def __init__(self,file:str, filepath:str = None, cwd:bool = False):
+
+        if filepath is None and not cwd:
+            path,filename = os.path.split(file)
+            if len(path) < 1 :
+                path = os.curdir
+        else:
+            filename = file
+            if cwd:
+                path = os.getcwd()
+            elif filepath is not None:
+                path = filepath
+            else:
+                path = os.curdir
+            
+        if os.path.isdir(path):
+            self.filepath = path
+        else:
+            raise NotADirectoryError
+
+        self.fullpath = os.path.join(path, filename)
+        if os.path.isfile(self.fullpath):
+            self.filename = filename
+            self.exists = True
+        else:
+            self.exists = False
+            raise FileNotFoundError
+
+        # full name with revision control: <name>.<ext>.<revision>
+        last = self.filename.rsplit(".",2)
+        
+        try:
+            self.revision = int(last[-1])>0
+            self.extension = last[-2]
+        except:
+            self.revision = False
+            self.extension = last[-1]
+    
+    def __repr__(self):
+        return f"{self.filename} | {self.fullpath}" 
+    
+    def __str__(self):
+        return f"{self.filename}"
+
+    @staticmethod
+    def isFile(file:str, filepath:str = None):
+        if filepath is None:
+            exist = os.path.isfile(file)
+        else:
+            exist = os.path.isfile(os.path.join(filepath,file))
+        return exist
+    
+    @staticmethod
+    def fileExtension(file:str):
+        # full name with revision control: <name>.<ext>.<revision>
+        last = file.rsplit(".",2)
+        
+        try:
+            if(int(last[-1])>0):
+                extension = last[-2]
+            else:
+                extension = last[-1]
+        except:
+            extension = last[-1]
+
+        return extension
+    
+    @staticmethod
+    def isExtension(file:str,extensions:str):
+        valid = list()
+        for ext in extensions:
+            clean_ext = ext.strip(".")
+            valid.append(fnmatch.fnmatch(File.fileExtension(file),ext.strip(".")))
+        return any(valid)
+
+    def removeRevision(self):
+        if self.revision:
+            newname = self.filename.rsplit(".",1)[0]
+            try:
+                os.rename(self.fullpath, os.path.join(self.filepath,newname))
+            except:
+                os.replace(self.fullpath, os.path.join(self.filepath,newname))
+            self.filename = newname
+            self.fullpath = os.path.join(self.filepath, newname)
+            self.revision = False
+        
 class Filemanager():
 
     def __init__(self, workingdirectory = False, extensions:str = "*"):
